90aa9f9df75ada21698167c9e89da1724cfd727b
adnanzaih <58091710+adnanzaih@users.noreply.github.com>
2021-03-14 10:52:11 -0600

    re-organized code presentation

--- P2 Task 1/T010_P2_channel-filters.py
+++ P2 Task 1/T010_P2_channel-filters.py
@@ -6,6 +6,20 @@ from Cimpl import *
 
 from unit_testing import check_equal
 
+#Function for red filter: Logan DeLaat-101182975
+def red_channel(image: Image) -> Image:
+    """
+    Logan DeLaat
+    Returns a copy of the image in png format which only contains the red components of the original image's pixel's colour
+    >>> image = load_image(choose_file())
+    >>> show(red_channel(image))
+    """
+
+    copy_image = copy(image) #creates copy of image to be changed
+    for x, y, (r, g, b) in image: #looks through all values of each pixel in the image
+        set_color(copy_image, x, y, create_color(r,0,0))  #set the colour to only contain the red value the pixel originally had
+    return copy_image
+
 #Green filter function: Vincent Chen-101196001
 def green_channel(image : Image)-> Image:
  """
@@ -23,89 +37,6 @@ def green_channel(image : Image)-> Image:
   set_color (green_image, x, y, create_color(0,g,0))
   
  return green_image #returns the filtered green image
-
-#Test Function for green filter: Vincent Chen-101196001
-def test_green() -> None:  
-    '''
-    Vincent Chen
-    A test function for green_channel. Uses a 4x1 pixel image as a sample to 
-    test functionality of the green filter.
-    >>> test_green()
-    '''   
-  
-    # Create image with test cases
-    
-    original = create_image(4, 1) 
-    set_color(original, 0, 0,  create_color(0, 0, 0))
-    set_color(original, 1, 0,  create_color(0, 110, 10))
-    set_color(original, 2, 0,  create_color(127, 127, 127))
-    set_color(original, 3, 0,  create_color(145, 75, 220))
-    
-    # Create an image that's identical to the one a correct implementation of
-    # green_channel should produce when it is passed original.
-    expected = create_image(4, 1) 
-    set_color(expected, 0, 0,  create_color(0, 0, 0))
-    set_color(expected, 1, 0,  create_color(0, 110, 0))
-    set_color(expected, 2, 0,  create_color(0, 127, 0))
-    set_color(expected, 3, 0,  create_color(0, 75, 0))
-
-       
-    # Now compare the transformed image returned by the filter with the
-    # expected image, one pixel at a time.
-    
-    green_image = green_channel(original)   #Using filter function on the original image
-    
-    for x, y, col in green_image: # col is the Color object for the pixel @ (x,y)
-                                 # There's no need to unpack that object into
-                                 # RGB components.
-        check_equal('Checking pixel @(' + str(x) + ', ' + str(y) + ')',
-                     col, get_color(expected, x, y))
-        
-
-#Function for red filter: Logan DeLaat-101182975
-def red_channel(image: Image) -> Image:
-    """
-    Logan DeLaat
-    Returns a copy of the image in png format which only contains the red components of the original image's pixel's colour
-    >>> image = load_image(choose_file())
-    >>> show(red_channel(image))
-    """
-
-    copy_image = copy(image) #creates copy of image to be changed
-    for x, y, (r, g, b) in image: #looks through all values of each pixel in the image
-        set_color(copy_image, x, y, create_color(r,0,0))  #set the colour to only contain the red value the pixel originally had      
-    return copy_image
-   
-#Test Function for red filter: Logan DeLaat-101182975
-def check_red() -> None:
-    """
-    Logan DeLaat
-    Tests the red_channel function to make sure the result is the same as expected
-    >>>check_red() 
-    """
-    #create small image with 6 pixels to test the function
-    test_image = create_image(6,1)
-    set_color(test_image, 0, 0, create_color(11, 22, 33))
-    set_color(test_image, 1, 0, create_color(55, 0, 133))
-    set_color(test_image, 2, 0, create_color(14, 72, 33))
-    set_color(test_image, 3, 0, create_color(78, 100, 2))
-    set_color(test_image, 4, 0, create_color(0, 0, 1))
-    set_color(test_image, 5, 0, create_color(44, 77, 66))
-
-    #creates a small image with 6 pixels with the expected outcome to compare the test image to
-    expected_image = create_image(6,1)
-    set_color(expected_image, 0, 0, create_color(11, 0, 0))
-    set_color(expected_image, 1, 0, create_color(55, 0, 0))
-    set_color(expected_image, 2, 0, create_color(14, 0, 0))
-    set_color(expected_image, 3, 0, create_color(78, 0, 0))
-    set_color(expected_image, 4, 0, create_color(0, 0, 0))
-    set_color(expected_image, 5, 0, create_color(44, 0, 0))
-
-    
-    #Comparing expected image to the image produced by red_channel 
-    for x, y, col in red_channel(test_image) : #goes through all the pixels and all their values
-        check_equal('Checking pixel @(' + str(x) + ', ' + str(y) + ')',
-                     col, get_color(expected_image, x, y)) #gets the colour of the pixel and compares it
 
 #Function for blue filter: Ayesha Dassanayake-101180472       
 def blue_channel(image: Image) -> Image:
@@ -125,35 +56,6 @@ def blue_channel(image: Image) -> Image:
         set_color(blue_img,x,y,blue)
         
     return blue_img
-
-#Test Function for blue filter: Ayesha Dassanayake-101180472  
-def test_blue() -> None:
-    """
-    Ayesha Dassanayake
-    Tests blue_channel
-    >>> test_blue()
-    """
-    
-    #Creates an image with 4 pixels
-    original = create_image(4,1)
-    set_color(original, 0, 0,  create_color(0, 0, 0))
-    set_color(original, 1, 0,  create_color(10, 20, 60))
-    set_color(original, 2, 0,  create_color(150, 0, 150))
-    set_color(original, 3, 0,  create_color(0, 81, 235))    
-    
-    #Expected transformation of image
-    expected = create_image(4, 1)
-    set_color(expected, 0, 0,  create_color(0, 0, 0))
-    set_color(expected, 1, 0,  create_color(0, 0, 60))
-    set_color(expected, 2, 0,  create_color(0, 0, 150))
-    set_color(expected, 3, 0,  create_color(0, 0, 235))    
-    
-    #Comparing expected image to the image produced by blue_channel
-    blue_img = blue_channel(original)   
-    for x, y, col in blue_img: 
-        check_equal('Checking pixel @(' + str(x) + ', ' + str(y) + ')',
-                     col, get_color(expected, x, y))   
-
 
 #Function for combined filter: Adnan Hafeez-101210710
 def combine(red: Image, green: Image, blue:Image) -> Image:
@@ -176,6 +78,104 @@ def combine(red: Image, green: Image, blue:Image) -> Image:
         combined_colors = create_color(red_pixel[0], green_pixel[1], blue_pixel[2])
         set_color(new_image, x,y, combined_colors)
     return new_image
+
+#Test Function for red filter: Logan DeLaat-101182975
+def check_red() -> None:
+    """
+    Logan DeLaat
+    Tests the red_channel function to make sure the result is the same as expected
+    >>>check_red()
+    """
+    #create small image with 6 pixels to test the function
+    test_image = create_image(6,1)
+    set_color(test_image, 0, 0, create_color(11, 22, 33))
+    set_color(test_image, 1, 0, create_color(55, 0, 133))
+    set_color(test_image, 2, 0, create_color(14, 72, 33))
+    set_color(test_image, 3, 0, create_color(78, 100, 2))
+    set_color(test_image, 4, 0, create_color(0, 0, 1))
+    set_color(test_image, 5, 0, create_color(44, 77, 66))
+
+    #creates a small image with 6 pixels with the expected outcome to compare the test image to
+    expected_image = create_image(6,1)
+    set_color(expected_image, 0, 0, create_color(11, 0, 0))
+    set_color(expected_image, 1, 0, create_color(55, 0, 0))
+    set_color(expected_image, 2, 0, create_color(14, 0, 0))
+    set_color(expected_image, 3, 0, create_color(78, 0, 0))
+    set_color(expected_image, 4, 0, create_color(0, 0, 0))
+    set_color(expected_image, 5, 0, create_color(44, 0, 0))
+
+
+    #Comparing expected image to the image produced by red_channel
+    for x, y, col in red_channel(test_image) : #goes through all the pixels and all their values
+        check_equal('Checking pixel @(' + str(x) + ', ' + str(y) + ')',
+                    col, get_color(expected_image, x, y)) #gets the colour of the pixel and compares it
+
+#Test Function for green filter: Vincent Chen-101196001
+def test_green() -> None:
+    '''
+    Vincent Chen
+    A test function for green_channel. Uses a 4x1 pixel image as a sample to
+    test functionality of the green filter.
+    >>> test_green()
+    '''
+
+    # Create image with test cases
+
+    original = create_image(4, 1)
+    set_color(original, 0, 0,  create_color(0, 0, 0))
+    set_color(original, 1, 0,  create_color(0, 110, 10))
+    set_color(original, 2, 0,  create_color(127, 127, 127))
+    set_color(original, 3, 0,  create_color(145, 75, 220))
+
+    # Create an image that's identical to the one a correct implementation of
+    # green_channel should produce when it is passed original.
+    expected = create_image(4, 1)
+    set_color(expected, 0, 0,  create_color(0, 0, 0))
+    set_color(expected, 1, 0,  create_color(0, 110, 0))
+    set_color(expected, 2, 0,  create_color(0, 127, 0))
+    set_color(expected, 3, 0,  create_color(0, 75, 0))
+
+
+    # Now compare the transformed image returned by the filter with the
+    # expected image, one pixel at a time.
+
+    green_image = green_channel(original)   #Using filter function on the original image
+
+    for x, y, col in green_image: # col is the Color object for the pixel @ (x,y)
+        # There's no need to unpack that object into
+        # RGB components.
+        check_equal('Checking pixel @(' + str(x) + ', ' + str(y) + ')',
+                    col, get_color(expected, x, y))
+
+#Test Function for blue filter: Ayesha Dassanayake-101180472
+def test_blue() -> None:
+    """
+    Ayesha Dassanayake
+    A test function for blue_channel. Uses a 4x1 pixel image as a sample to
+    test functionality of the blue filter.
+    >>> test_blue()
+    """
+
+    #Creates an image with 4 pixels
+    original = create_image(4,1)
+    set_color(original, 0, 0,  create_color(0, 0, 0))
+    set_color(original, 1, 0,  create_color(10, 20, 60))
+    set_color(original, 2, 0,  create_color(150, 0, 150))
+    set_color(original, 3, 0,  create_color(0, 81, 235))
+
+    #Expected transformation of image
+    expected = create_image(4, 1)
+    set_color(expected, 0, 0,  create_color(0, 0, 0))
+    set_color(expected, 1, 0,  create_color(0, 0, 60))
+    set_color(expected, 2, 0,  create_color(0, 0, 150))
+    set_color(expected, 3, 0,  create_color(0, 0, 235))
+
+    #Comparing expected image to the image produced by blue_channel
+    blue_img = blue_channel(original)
+    for x, y, col in blue_img:
+        check_equal('Checking pixel @(' + str(x) + ', ' + str(y) + ')',
+                    col, get_color(expected, x, y))
+
 
 #Test function for combined filter: Adnan Hafeez-101210710
 def test_combine() -> None:
@@ -263,7 +263,7 @@ show(new_image)
 print("Testing blue filter:")
 test_blue() 
 
-#main script combined
+#main script combine
 combined = combine(red_channel(image), green_channel(image), blue_channel(image))
 show(combined)
 print("Testing combine filter:")
