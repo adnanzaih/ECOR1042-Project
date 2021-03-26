5d6087fc5f5b702d4d28d78294d329cb298f7c88
addassa <64609969+addassa@users.noreply.github.com>
2021-03-26 18:52:28 -0400

    Finished code, need to add comments

--- Milestone 2/P4 Task 1/T010_P4_filter_edge.py
+++ Milestone 2/P4 Task 1/T010_P4_filter_edge.py
@@ -1,6 +1,6 @@
 #Ayesha Dassanayake T010 101180472
 
-from Cimpl import choose_file, load_image, show, Image, copy, create_color, set_color, get_height
+from Cimpl import choose_file, load_image, show, Image, copy, create_color, set_color, get_height, get_color
 
 def detect_edges(image: Image, threshold: int) -> Image:
 
@@ -9,7 +9,7 @@ def detect_edges(image: Image, threshold: int) -> Image:
     
     Returns a new image with black and white pixels depending if the contrast is greater or less than the threshold
     >>> image = load_image(choose_file())
-    >>> new_image = detect_edges(image,15)
+    >>> new_image = detect_edges(image,13)
     >>> show(new_image)
     """    
     
@@ -21,13 +21,12 @@ def detect_edges(image: Image, threshold: int) -> Image:
     height = get_height(edge_img)
     
     for x,y,(r,g,b) in edge_img:
-        og_color = create_color(r,g,b)
-        top_pix = set_color(edge_img,x,y)
+        top_pix = get_color(image,x,y)
         rtop,gtop,btop = top_pix
         
         if y < height - 1:
-            btm_pix = set_color(image,x,y+1,og_color)
-            rbtm,gbtm,bbtm, btm_pix
+            btm_pix = get_color(image,x,y+1)
+            rbtm,gbtm,bbtm = btm_pix
             
             top_avg = (rtop + gtop + btop)/3
             btm_avg = (rbtm + gbtm + bbtm)/3
@@ -36,6 +35,8 @@ def detect_edges(image: Image, threshold: int) -> Image:
             
             if contrast > threshold:
                 set_color(edge_img,x,y,black_pix)
+            else:
+                set_color(edge_img,x,y,white_pix)
                 
         else: 
             set_color(edge_img,x,y,white_pix)
@@ -44,5 +45,5 @@ def detect_edges(image: Image, threshold: int) -> Image:
 
 if __name__ == "__main__":
     image = load_image(choose_file())
-    new_image = detect_edges(image,15)
+    new_image = detect_edges(image,13)
     show(new_image)        
