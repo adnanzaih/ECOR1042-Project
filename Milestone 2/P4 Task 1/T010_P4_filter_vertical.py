d094cf36a9b5e099f2523447013fd4390ea6eddc
addassa <64609969+addassa@users.noreply.github.com>
2021-03-27 17:41:37 -0400

    Fixed grammatical errors

--- Milestone 2/P4 Task 1/T010_P4_filter_vertical.py
+++ Milestone 2/P4 Task 1/T010_P4_filter_vertical.py
@@ -2,13 +2,13 @@
 
 from Cimpl import Image, set_color, create_color, load_image, choose_file, show, copy, get_height
 
-def flip_veritcal(image: Image)-> Image:
-    """Returns a copy of the image with that is flipped over an imaginary horizontal line in the centre of the image
+def flip_vertical(image: Image)-> Image:
+    """Returns a copy of the image that is flipped over an imaginary horizontal line in the centre of the image
     >>>file = load_image(choose_file())
     >>>show(flip_vertical(file))
     """
     
-    bottom = get_height(image) #This is needed to find the total height of the image so that the distance the pixel need to be "swaped" can be caluclated. 
+    bottom = get_height(image) #This is needed to find the total height of the image so that the distance the pixel needs to be "swapped" can be calculated. 
     copy_image = copy(image)
     for x, y, (r, g, b) in image:
         set_color(copy_image, x, (bottom-y-1), create_color(r,g,b)) #Replaces pixel's colour with the colour of the pixel equal distance away
