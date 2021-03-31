2912368832a2dba2a75e2d6036603d0a1698e180
adnanzaih <58091710+adnanzaih@users.noreply.github.com>
2021-03-31 00:49:58 -0600

    milestone 3

---
+++ Milestone 3/T010_interactive_ui.py
@@ -0,0 +1,56 @@
+from T010_image_filters import *
+from Cimpl import *
+
+options = "L)oad Image    S)ave-as \n3)-tone    X)treme contrast    T)int sepia    P)osterize \nE)dge detect    D)raw curve    V)ertical flip    H)orizontal flip \nQ)uit \n:"
+valid_commands = ["L","S","3","X","T","P","E","D","V","H","Q"]
+command = ""
+load_img = []
+print(options)
+while command.upper() != "Q":
+    command = ""
+    command = input()
+    if command.upper() == "L":
+        load_img = load_image(choose_file())
+    if bool(load_img) == True: #image is loaded
+        if command.upper() == "3":
+            load_img = three_tone(load_img,"aqua","blood","lemon")
+            show(load_img)
+        elif command.upper() == "X":
+            load_img = extreme_contrast(load_img)
+            show(load_img)
+        elif command.upper() == "T":
+            load_img = sepia_filter(load_img)
+            show(load_img)
+        elif command.upper() == "P":
+            load_img = posterize(load_img)
+            show(load_img)
+        elif command.upper() == "V":
+            load_img = flip_vertical(load_img)
+            show(load_img)
+        elif command.upper() == "H":
+            load_img = flip_horizontal(load_img)
+            show(load_img)
+        elif command.upper() == "E":
+            threshold = input("Please input a threshold value between 0-255: ")
+            load_img = detect_edges(load_img, int(threshold))
+            show(load_img)
+        elif command.upper() == "D":
+            load_img = draw_curve(load_img,"aqua", pointList=None)[0]
+            show(load_img)
+        elif command.upper() == "S":
+            load_img = load_image(choose_file())
+
+        elif command.upper() not in valid_commands:
+            print("no such command")
+
+    if bool(load_img) == False:
+        if command.upper() in valid_commands:
+            if command.upper() != "Q" or command.upper() != "L":
+                print("No image loaded")
+
+        if command.upper() not in valid_commands:
+            print("no such command")
+
+    print(options)
+print("Quitting program")
+
